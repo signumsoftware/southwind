b270ac50dbf16eac819ada37f7dd7cfdd63f4a79
Olmo del Corral <olmo@signumsoftware.com>
2013-06-18 15:54:42 +0200

    add ckeditor

---
+++ Southwind.Web/Scripts/ckeditor/config.js
@@ -0,0 +1,25 @@
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+    config.uiColor = '#FAFAFA';
+
+    config.toolbar = 'MyToolbar';
+
+    config.toolbar_MyToolbar =
+	[
+		{ name: 'document', items: ['Source'] },
+		{ name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'] },
+		{ name: 'editing', items: ['SelectAll', '-', 'Scayt'] },
+		{ name: 'insert', items: ['Link', 'Unlink', 'Image', 'Table', 'HorizontalRule', 'SpecialChar'] }, 
+        '/',
+		{ name: 'styles', items: ['Format', 'Font', 'FontSize'] },
+		{ name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', '-', 'RemoveFormat'] },
+		{ name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote'] },
+		{ name: 'tools', items: ['Preview', '-', 'Maximize'] }
+	];
+};
