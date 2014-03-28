fba3051bd3d4d49b47a629a8eeb6eba2f2be4240
Olmo del Corral <olmo@signumsoftware.com>
2014-03-28 13:16:16 +0100

    update ckeditor

--- Southwind.Web/Libs/ckeditor/build-config.js
+++ Southwind.Web/Libs/ckeditor/build-config.js
@@ -1,21 +1,32 @@
-﻿
-/**
+﻿/**
  * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 /**
  * This file was added automatically by CKEditor builder.
- * You may re-use it at any time at http://ckeditor.com/builder to build CKEditor again.
- * 
- * NOTE: 
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/c48acd67e48a23110d612453544d8176
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/c48acd67e48a23110d612453544d8176
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
  *    This file is not used by CKEditor, you may remove it.
  *    Changing this file will not change your CKEditor configuration.
  */
 
 var CKBUILDER_CONFIG = {
-	skin: 'moono',
-	preset: 'full',
+	skin: 'bootstrapck',
+	preset: 'standard',
 	ignore: [
 		'dev',
 		'.gitignore',
@@ -23,67 +34,47 @@ var CKBUILDER_CONFIG = {
 		'README.md',
 		'.mailmap'
 	],
-	plugins: {
-		'dialogadvtab' : 1,
+	plugins : {
+		'a11yhelp' : 1,
+		'about' : 1,
 		'basicstyles' : 1,
 		'blockquote' : 1,
 		'clipboard' : 1,
-		'colorbutton' : 1,
+		'codemirror' : 1,
 		'colordialog' : 1,
-		'templates' : 1,
 		'contextmenu' : 1,
-		'resize' : 1,
-		'toolbar' : 1,
 		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
 		'filebrowser' : 1,
 		'floatingspace' : 1,
-		'font' : 1,
 		'format' : 1,
-		'htmlwriter' : 1,
 		'horizontalrule' : 1,
-		'iframe' : 1,
-		'wysiwygarea' : 1,
+		'htmlwriter' : 1,
 		'image' : 1,
-		'indent' : 1,
-		'justify' : 1,
+		'indentlist' : 1,
 		'link' : 1,
 		'list' : 1,
 		'liststyle' : 1,
 		'magicline' : 1,
 		'maximize' : 1,
-		'pagebreak' : 1,
-		'pastetext' : 1,
 		'pastefromword' : 1,
-		'preview' : 1,
+		'pastetext' : 1,
+		'pbckcode' : 1,
 		'removeformat' : 1,
-		'selectall' : 1,
+		'resize' : 1,
+		'scayt' : 1,
 		'showblocks' : 1,
-		'showborders' : 1,
 		'sourcearea' : 1,
 		'specialchar' : 1,
-		'scayt' : 1,
 		'stylescombo' : 1,
 		'tab' : 1,
 		'table' : 1,
 		'tabletools' : 1,
+		'toolbar' : 1,
 		'undo' : 1,
 		'wsc' : 1,
-		'dialog' : 1,
-		'dialogui' : 1,
-		'panelbutton' : 1,
-		'button' : 1,
-		'floatpanel' : 1,
-		'panel' : 1,
-		'menu' : 1,
-		'popup' : 1,
-		'richcombo' : 1,
-		'listblock' : 1,
-		'fakeobjects' : 1,
-		'menubutton' : 1,
-		'codemirror' : 1,
-		'indentlist' : 1
+		'wysiwygarea' : 1
 	},
 	languages : {
 		'en' : 1,
