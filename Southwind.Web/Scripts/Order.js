9f04a835ed294a89106966e856c6a363b4438d89
Olmo del Corral <olmo@signumsoftware.com>
2014-07-23 23:34:09 +0200

    some TypeScript magin on Order.cshtml

--- Southwind.Web/Scripts/Order.js
+++ Southwind.Web/Scripts/Order.js
@@ -23,10 +23,62 @@ define(["require", "exports", "Framework/Signum.Web/Signum/Scripts/Finder"], fun
     }
     exports.attachCustomerEntityLine = attachCustomerEntityLine;
 
+    function attachProductLine(el, fo) {
+        el.entityChanged = function () {
+            el.getOrRequestEntityHtml().then(function (e) {
+                var root = el.prefix.parent("Product");
+
+                root.child("UnitPrice").get().val(e.getChild("UnitPrice").val() || e.getChild("UnitPrice").text());
+                root.child("Quantity").get().val("1");
+                root.child("Quantity").get().trigger("change");
+            });
+        };
+    }
+    exports.attachProductLine = attachProductLine;
+
+    function attachDetails(el, comma) {
+        el.element.on("change", "div.recalculate input", function (e) {
+            reCalculate(el, comma);
+        });
+
+        el.entityChanged = function () {
+            return reCalculate(el, comma);
+        };
+    }
+    exports.attachDetails = attachDetails;
+
+    function reCalculate(ed, comma) {
+        var total = 0;
+        ed.element.find(".sf-repeater-element").each(function (i, e) {
+            var prefix = e.id.parent("sfRepeaterItem");
+
+            var unitPrice = parseFloatLoc(prefix.child("UnitPrice").get().val(), comma);
+            var quantity = parseFloatLoc(prefix.child("Quantity").get().val(), comma);
+            var discount = parseFloatLoc(prefix.child("Discount").get().val().replace("%", ""), comma);
+
+            var subTotalPrice = unitPrice * quantity * (1 - discount / 100);
+
+            if (!isNaN(subTotalPrice))
+                prefix.child("SubTotalPrice").get().text(toStringLoc(subTotalPrice, comma));
+
+            total += subTotalPrice;
+        });
+
+        ed.prefix.parent().child("TotalPrice").get().text(toStringLoc(total, comma));
+    }
+
+    function parseFloatLoc(str, comma) {
+        return parseFloat(str.replace(comma, "."));
+    }
+
+    function toStringLoc(val, comma) {
+        return Number(val).toFixed(2).replace(".", comma);
+    }
+
     function updateStockValue(prefix) {
         var sum = parseFloat(prefix.child("UnitPrice").get().val()) * parseFloat(prefix.child("UnitsInStock").get().val());
 
-        prefix.child("ValueInStock_sfStaticValue").get().html(sum.toString());
+        prefix.child("ValueInStock").get().html(sum.toString());
     }
     exports.updateStockValue = updateStockValue;
 });
--- Southwind.Web/Scripts/Order.ts
+++ Southwind.Web/Scripts/Order.ts
@@ -27,9 +27,59 @@ export function attachCustomerEntityLine(el: Lines.EntityLine, fo: Finder.FindOp
     };
 }
 
+export function attachProductLine(el: Lines.EntityLine, fo: Finder.FindOptions) {
+    el.entityChanged = () => {
+
+        el.getOrRequestEntityHtml().then(e=> {
+
+            var root = el.prefix.parent("Product");
+
+            root.child("UnitPrice").get().val(e.getChild("UnitPrice").val() || e.getChild("UnitPrice").text());
+            root.child("Quantity").get().val("1");
+            root.child("Quantity").get().trigger("change");
+        });
+    };
+}
+
+export function attachDetails(el: Lines.EntityLineDetail, comma: string) {
+    el.element.on("change", "div.recalculate input", e=> {
+        reCalculate(el, comma);
+    }); 
+
+    el.entityChanged = () => reCalculate(el, comma);
+}
+
+function reCalculate(ed: Lines.EntityLineDetail, comma: string) {
+    var total = 0;
+    ed.element.find(".sf-repeater-element").each((i, e) => {
+        var prefix = (<HTMLElement>e).id.parent("sfRepeaterItem");
+
+        var unitPrice = parseFloatLoc(prefix.child("UnitPrice").get().val(), comma);
+        var quantity = parseFloatLoc(prefix.child("Quantity").get().val(), comma);
+        var discount = parseFloatLoc(prefix.child("Discount").get().val().replace("%", ""), comma);
+
+        var subTotalPrice = unitPrice * quantity * (1 - discount / 100);  
+
+        if (!isNaN(subTotalPrice))
+            prefix.child("SubTotalPrice").get().text(toStringLoc(subTotalPrice, comma));
+
+        total += subTotalPrice;
+    });
+
+    ed.prefix.parent().child("TotalPrice").get().text(toStringLoc(total, comma));
+}
+
+function parseFloatLoc(str: string, comma: string) {
+    return parseFloat(str.replace(comma, "."));
+}
+
+function toStringLoc(val: number, comma: string) {
+    return Number(val).toFixed(2).replace(".", comma);
+}
+
 export function updateStockValue(prefix: string) {
     var sum = parseFloat(prefix.child("UnitPrice").get().val()) *
               parseFloat(prefix.child("UnitsInStock").get().val());
 
-    prefix.child("ValueInStock_sfStaticValue").get().html(sum.toString());
+    prefix.child("ValueInStock").get().html(sum.toString());
 }
