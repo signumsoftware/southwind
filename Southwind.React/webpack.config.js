594cc79e74c6bef15d1bb40a7b47d8c84fc463ee
Olmo del Corral <olmo@signumsoftware.com>
2016-04-07 21:13:25 +0200

    optimize webpack

--- Southwind.React/webpack.config.js
+++ Southwind.React/webpack.config.js
@@ -5,8 +5,8 @@ var webpack = require('webpack');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 var AssetsPlugin = require('assets-webpack-plugin');
 var BeepPlugin = require('webpack-beep-plugin');
+var node_modules = path.join(__dirname, "node_modules");
 
-console.log(process.version);
 
 module.exports = {
     entry: {
@@ -17,8 +17,12 @@ module.exports = {
             "../Framework/Signum.React/Scripts/Frames/ModalFrame",
             "../Framework/Signum.React/Scripts/SearchControl/SearchPage", 
             "../Framework/Signum.React/Scripts/SearchControl/SearchModal"],
-        "react": ["react", "react-bootstrap", "react-router", "react-widgets", "react-router-bootstrap"],
-        "moment": ["moment"],
+        "react": ["react", "react-bootstrap", "react-router", "react-widgets", "react-router-bootstrap", "moment"],
+        chart: [
+            "../Extensions/Signum.React.Extensions/Chart/ChartScript/ChartScript.tsx",
+            "../Extensions/Signum.React.Extensions/Chart/Templates/ChartRequestView.tsx",
+            "../Extensions/Signum.React.Extensions/Chart/UserChart/UserChart.tsx"
+        ]
         //"lines": []
         //"d3": ["d3"],
     },
@@ -28,13 +32,21 @@ module.exports = {
         chunkFilename: "bundle.[name].[chunkhash].js"
     },
     resolve: {
-        root: path.join(__dirname, "node_modules"),
-        extensions: ['', '.Webpack.js', '.web.js', '.ts', '.js', '.tsx']
+        root: node_modules,
+        extensions: ['', '.Webpack.js', '.web.js', '.ts', '.js', '.tsx']     
     },
     resolveLoader: { root: path.join(__dirname, "node_modules") },
     module: {
+        //noParse : [
+        //    //node_modules + "/react/dist/react.min.js",
+        //    node_modules + "/react-dom/dist/react-dom.min.js",
+        //    node_modules + "/react-bootstrap/dist/react-bootstrap.min.js",
+        //    node_modules + "/react-router/umd/ReactRouter.min.js",
+        //    node_modules + "/react-widgets/dist/react-widgets.js"
+        //],
         loaders: [
-            { test: /\.tsx?$/, loader: 'ts-loader' },
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+            //{ test: /\.tsx?$/, loader: 'happypack/loader' },
             //{ test: /\.jsx?$/, loader: "babel-loader" }
             { test: /\.css$/, loader: "style-loader!css-loader" },
             { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
@@ -45,9 +57,10 @@ module.exports = {
     },
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
-            names: ['react', 'moment'],
+            names: ['chart', 'main', 'react'],
             minChunks : Infinity
         }),
+        //new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /(en|es|fr|de|it)\.js/),
         new CleanWebpackPlugin(['dist'], {
             //root: '/full/project/path',
             verbose: true,
