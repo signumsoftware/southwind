2d4455197cf4b83955f1e85dcc7b965fa0e0bf77
Olmo del Corral <olmo@signumsoftware.com>
2016-05-04 21:45:52 +0200

    add NotFound

--- Southwind.React/Scripts/NotFound.tsx
+++ Southwind.React/Scripts/NotFound.tsx
@@ -4,6 +4,10 @@ import * as React from 'react'
 
 export default class NotFound extends React.Component<{}, {}> {
     render() {
-        return (<div>Not Found JUHU</div>);
+        return (
+            <div>
+                <h3>404 <small>Not Found</small></h3>
+            </div>
+        );
     }
 }
