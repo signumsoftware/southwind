645454f906c852f39e0e6da0ccec2c0ea411e017
Olmo del Corral <olmo@signumsoftware.com>
2016-06-05 22:12:00 +0200

    tabs in ApplicationConfiguration.tsx

--- Southwind.React/Scripts/Southwind/Templates/ApplicationConfiguration.tsx
+++ Southwind.React/Scripts/Southwind/Templates/ApplicationConfiguration.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
+import { Tabs, Tab} from 'react-bootstrap'
 import { ApplicationConfigurationEntity } from '../Southwind.Entities'
-import { ValueLine, EntityLine, EntityCombo, EntityList, EntityDetail, EntityStrip, EntityRepeater, TypeContext } from '../../../../Framework/Signum.React/Scripts/Lines'
+import { ValueLine, EntityLine, EntityCombo, EntityList, EntityDetail, EntityStrip, EntityRepeater, TypeContext, RenderEntity } from '../../../../Framework/Signum.React/Scripts/Lines'
 
 export default class ApplicationConfiguration extends React.Component<{ ctx: TypeContext<ApplicationConfigurationEntity> }, void> {
 
@@ -8,10 +9,16 @@ export default class ApplicationConfiguration extends React.Component<{ ctx: Typ
         var ctx = this.props.ctx;
         return (
             <div>
-                <ValueLine ctx={ctx.subCtx(a => a.environment)} />
-                <EntityDetail ctx={ctx.subCtx(a => a.email)} />
-                <EntityLine ctx={ctx.subCtx(a => a.smtpConfiguration)} />
-                <EntityDetail ctx={ctx.subCtx(a => a.sms)} />
+                <ValueLine ctx={ctx.subCtx(a => a.environment) } />
+                <Tabs>
+                    <Tab eventKey="tab" title={ctx.niceName(a => a.email) }>
+                        <RenderEntity ctx={ctx.subCtx(a => a.email) } />
+                        <EntityLine ctx={ctx.subCtx(a => a.smtpConfiguration) } />
+                    </Tab>
+                    <Tab eventKey="sms" title={ctx.niceName(a => a.sms) }>
+                        <EntityDetail ctx={ctx.subCtx(a => a.sms) } />
+                    </Tab>
+                </Tabs>
             </div>
         );
     }
