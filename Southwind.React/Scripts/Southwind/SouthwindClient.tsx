007bfe2feef0e6d30d9c92b23ea24907fbe5b7eb
Olmo del Corral <olmo@signumsoftware.com>
2016-09-07 20:43:53 +0200

    update Framework

--- Southwind.React/Scripts/Southwind/SouthwindClient.tsx
+++ Southwind.React/Scripts/Southwind/SouthwindClient.tsx
@@ -2,12 +2,12 @@ import * as React from 'react'
 import { Route } from 'react-router'
 import * as moment from 'moment'
 import { ajaxPost, ajaxGet } from '../../../Framework/Signum.React/Scripts/Services';
-import { EntitySettings } from '../../../Framework/Signum.React/Scripts/Navigator'
+import { EntitySettings, ViewPromise } from '../../../Framework/Signum.React/Scripts/Navigator'
 import * as Navigator from '../../../Framework/Signum.React/Scripts/Navigator'
 import * as Finder from '../../../Framework/Signum.React/Scripts/Finder'
 import { EntityOperationSettings, ConstructorOperationSettings, ContextualOperationSettings } from '../../../Framework/Signum.React/Scripts/Operations'
 import * as Operations from '../../../Framework/Signum.React/Scripts/Operations'
-import { navigateOrTab, defaultContextualClick } from '../../../Framework/Signum.React/Scripts/Operations/ContextualOperations'
+import { defaultContextualClick } from '../../../Framework/Signum.React/Scripts/Operations/ContextualOperations'
 import { defaultExecuteEntity } from '../../../Framework/Signum.React/Scripts/Operations/EntityOperations'
 
 import { getMixin } from '../../../Framework/Signum.React/Scripts/Signum.Entities'
@@ -18,27 +18,29 @@ import ValueLineModal from '../../../Framework/Signum.React/Scripts/ValueLineMod
 
 import OrderFilter from './Templates/OrderFilter'
 
+import { ApplicationConfigurationEntity } from './Southwind.Entities'
 
-import { AddressEntity, OrderDetailsEntity, OrderFilterModel, ApplicationConfigurationEntity, CategoryEntity,
+import { AddressEntity, OrderDetailsEntity, OrderFilterModel, CategoryEntity,
     CompanyEntity, EmployeeEntity, OrderEntity, PersonEntity, ProductEntity,
-    RegionEntity, ShipperEntity, SupplierEntity, TerritoryEntity, UserEmployeeMixin, OrderOperation, CustomerEntity } from './Southwind.Entities'
+    RegionEntity, ShipperEntity, SupplierEntity, TerritoryEntity, UserEmployeeMixin, OrderOperation, CustomerEntity
+} from './Southwind.Entities'
 
 export function start(options: { routes: JSX.Element[] }) {
 
-    Navigator.addSettings(new EntitySettings(AddressEntity, a => new Promise(resolve => require(['./Templates/Address'], resolve))));
-    Navigator.addSettings(new EntitySettings(OrderDetailsEntity, o => new Promise(resolve => require(['./Templates/OrderDetails'], resolve))));
-    Navigator.addSettings(new EntitySettings(OrderFilterModel, o => new Promise(resolve => require(['./Templates/OrderFilter'], resolve))));
-    Navigator.addSettings(new EntitySettings(ApplicationConfigurationEntity, a => new Promise(resolve => require(['./Templates/ApplicationConfiguration'], resolve))));
-    Navigator.addSettings(new EntitySettings(CategoryEntity, c => new Promise(resolve => require(['./Templates/Category'], resolve))));
-    Navigator.addSettings(new EntitySettings(CompanyEntity, c => new Promise(resolve => require(['./Templates/Company'], resolve))));
-    Navigator.addSettings(new EntitySettings(EmployeeEntity, e => new Promise(resolve => require(['./Templates/Employee'], resolve))));
-    Navigator.addSettings(new EntitySettings(OrderEntity, o => new Promise(resolve => require(['./Templates/Order'], resolve))));
-    Navigator.addSettings(new EntitySettings(PersonEntity, p => new Promise(resolve => require(['./Templates/Person'], resolve))));
-    Navigator.addSettings(new EntitySettings(ProductEntity, p => new Promise(resolve => require(['./Templates/Product'], resolve))));
-    Navigator.addSettings(new EntitySettings(RegionEntity, r => new Promise(resolve => require(['./Templates/Region'], resolve))));
-    Navigator.addSettings(new EntitySettings(ShipperEntity, s => new Promise(resolve => require(['./Templates/Shipper'], resolve))));
-    Navigator.addSettings(new EntitySettings(SupplierEntity, s => new Promise(resolve => require(['./Templates/Supplier'], resolve))));
-    Navigator.addSettings(new EntitySettings(TerritoryEntity, t => new Promise(resolve => require(['./Templates/Territory'], resolve))));
+    Navigator.addSettings(new EntitySettings(ApplicationConfigurationEntity, a => new ViewPromise(resolve => require(['./Templates/ApplicationConfiguration'], resolve))));
+    Navigator.addSettings(new EntitySettings(AddressEntity, a => new ViewPromise(resolve => require(['./Templates/Address'], resolve))));
+    Navigator.addSettings(new EntitySettings(OrderDetailsEntity, o => new ViewPromise(resolve => require(['./Templates/OrderDetails'], resolve))));
+    Navigator.addSettings(new EntitySettings(OrderFilterModel, o => new ViewPromise(resolve => require(['./Templates/OrderFilter'], resolve))));
+    Navigator.addSettings(new EntitySettings(CategoryEntity, c => new ViewPromise(resolve => require(['./Templates/Category'], resolve))));
+    Navigator.addSettings(new EntitySettings(CompanyEntity, c => new ViewPromise(resolve => require(['./Templates/Company'], resolve))));
+    Navigator.addSettings(new EntitySettings(EmployeeEntity, e => new ViewPromise(resolve => require(['./Templates/Employee'], resolve))));
+    Navigator.addSettings(new EntitySettings(OrderEntity, o => new ViewPromise(resolve => require(['./Templates/Order'], resolve))));
+    Navigator.addSettings(new EntitySettings(PersonEntity, p => new ViewPromise(resolve => require(['./Templates/Person'], resolve))));
+    Navigator.addSettings(new EntitySettings(ProductEntity, p => new ViewPromise(resolve => require(['./Templates/Product'], resolve))));
+    Navigator.addSettings(new EntitySettings(RegionEntity, r => new ViewPromise(resolve => require(['./Templates/Region'], resolve))));
+    Navigator.addSettings(new EntitySettings(ShipperEntity, s => new ViewPromise(resolve => require(['./Templates/Shipper'], resolve))));
+    Navigator.addSettings(new EntitySettings(SupplierEntity, s => new ViewPromise(resolve => require(['./Templates/Supplier'], resolve))));
+    Navigator.addSettings(new EntitySettings(TerritoryEntity, t => new ViewPromise(resolve => require(['./Templates/Territory'], resolve))));
 
     Navigator.getSettings(UserEntity).overrideView((rep) => {
         rep.insertAfter(u => u.role,
@@ -78,7 +80,7 @@ export function start(options: { routes: JSX.Element[] }) {
                     return undefined;
 
                 return Operations.API.constructFromMany(coc.context.lites, coc.operationInfo.key, c)
-                    .then(ep => navigateOrTab(ep, e))
+                    .then(ep => Navigator.createNavigateOrTab(ep, e))
                     .done();
             }).done();
         }
@@ -86,7 +88,7 @@ export function start(options: { routes: JSX.Element[] }) {
 
     const selectShippedDate = () => ValueLineModal.show({
         type: { name: "datetime" },
-        value: moment().format(),
+        initialValue: moment().format(),
         labelText: OrderEntity.nicePropertyName(a => a.shippedDate)
     });
 
@@ -104,6 +106,6 @@ export function start(options: { routes: JSX.Element[] }) {
                     .done();
             }
         }
-    }));
+    }));//Ship
 }
 
