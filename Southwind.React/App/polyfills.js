4515119b9684724be86c96cd6d4ae9fffd46009b
Olmo del Corral <olmo@signumsoftware.com>
2020-06-14 21:21:26 +0200

    update Framework and Extensions, remove query-string, add url-search-params-polyfill and extract polyfills.js

---
+++ Southwind.React/App/polyfills.js
@@ -0,0 +1,5 @@
+//polyfills
+require('es6-promise/auto');
+require('whatwg-fetch');
+require('abortcontroller-polyfill');
+require('url-search-params-polyfill')
--- Southwind.React/App/vendors.js
+++ Southwind.React/App/vendors.js
@@ -1,8 +1,3 @@
-//polyfills
-require('es6-promise/auto');
-require('whatwg-fetch');
-require('abortcontroller-polyfill');
-
 //third-pary libs
 require("react");
 require("react-dom");
@@ -10,7 +5,6 @@ require("prop-types");
 require("react-router");
 require("react-widgets");
 require("react-bootstrap");
-require("query-string");
 require("moment");
 require("numbro");
 require("@fortawesome/fontawesome-svg-core");
