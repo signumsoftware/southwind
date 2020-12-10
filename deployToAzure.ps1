04957e6ce007a70ba7e2b28ad0022b01cde41914
Olmo del Corral <olmo@signumsoftware.com>
2020-12-10 22:31:15 +0100

    Upgrade_20201126_AddWebAppRestart

--- deployToAzure.ps1
+++ deployToAzure.ps1
@@ -2,3 +2,4 @@ docker build -f ".\Southwind.React\Dockerfile" . -t southwind
 docker tag southwind southwind.azurecr.io/signum/southwind
 az acr login --name southwind
 docker push southwind.azurecr.io/signum/southwind
+az webapp restart --name southwind-webapp --resource-group southwind-resourceGroup
